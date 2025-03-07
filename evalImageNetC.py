9d14014cc6c4b814fc7cb1bb4868d0cc93df0862
Huiyuan Miao <126112893+Huiyuan-Miao@users.noreply.github.com>
2025-03-07 00:14:00 -0600

    Update evalImageNetC.py

--- evalImageNetC.py
+++ evalImageNetC.py
@@ -1,7 +1,4 @@
-''' Summary
-To train AlexNet to fit Cadena's nerual data.
-'''
-
+# to test the model's ability to different types of corruption using dataset imagenet - C
 import os
 import sys
 import random
